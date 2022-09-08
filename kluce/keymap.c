07e01b0e8d12515b15bcf7a36c5796dc0a4ea5e1
Filip Kučerák <xkucerak@fi.muni.cz>
2022-09-08 11:10:26 +0200

    firmware: Change the timed modifier from right ctrl to left ctrl

--- kluce/keymap.c
+++ kluce/keymap.c
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_CAPS,KC_LCTRL,KC_LGUI, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, RCTL_T(KC_CAPS), KC_LALT, KC_RGUI
+                 KC_CAPS,KC_LCTRL,KC_LGUI, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, LCTL_T(KC_CAPS), KC_LALT, KC_RGUI
 ),
 
 /*
@@ -157,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                       KC_F,    KC_G,    KC_C,    KC_R,    KC_L,  KC_BSPC,
   KC_ESC,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                       KC_D,    KC_H,    KC_T,    KC_N,    KC_S,  KC_MINS,
   KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_MUTE,  XXXXXXX, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  KC_RSFT,
-                 KC_CAPS,KC_LCTRL,KC_LGUI, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, RCTL_T(KC_CAPS), KC_LALT, KC_RGUI
+                 KC_CAPS,KC_LCTRL,KC_LGUI, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, LCTL_T(KC_CAPS), KC_LALT, KC_RGUI
 ),
 
 
